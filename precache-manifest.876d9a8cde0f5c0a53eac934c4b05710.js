354b31f94daf33820a2058708365a2fedf96a840
saiteja623 <saiteja.balla13@gmail.com>
2020-07-05 16:22:20 +0530

    Updates

---
+++ precache-manifest.876d9a8cde0f5c0a53eac934c4b05710.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf05d7ed8f837536e2f0d650ea61cecb",
+    "url": "/material-ui-design/index.html"
+  },
+  {
+    "revision": "91e20f3f95b5687bb74d",
+    "url": "/material-ui-design/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "4250eb061267b73de9e7",
+    "url": "/material-ui-design/static/css/main.7e2654f0.chunk.css"
+  },
+  {
+    "revision": "91e20f3f95b5687bb74d",
+    "url": "/material-ui-design/static/js/2.9a072d40.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-ui-design/static/js/2.9a072d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4250eb061267b73de9e7",
+    "url": "/material-ui-design/static/js/main.b0f8977b.chunk.js"
+  },
+  {
+    "revision": "2363cc4a1236b1d326e5",
+    "url": "/material-ui-design/static/js/runtime-main.899f215d.js"
+  },
+  {
+    "revision": "db511e4740bd4b36b9dd4679ed463a54",
+    "url": "/material-ui-design/static/media/pic1.db511e47.jpg"
+  },
+  {
+    "revision": "f422f469b7a389d439143b3b78fb31ac",
+    "url": "/material-ui-design/static/media/pic10.f422f469.jpg"
+  },
+  {
+    "revision": "ed11748e98acf6f104a12c7e0061b3ae",
+    "url": "/material-ui-design/static/media/pic11.ed11748e.jpg"
+  },
+  {
+    "revision": "3afcddfb649491391d28110d12aec0e2",
+    "url": "/material-ui-design/static/media/pic12.3afcddfb.jpg"
+  },
+  {
+    "revision": "4e9339bb8830918a84aa4094dc66fd15",
+    "url": "/material-ui-design/static/media/pic2.4e9339bb.jpg"
+  },
+  {
+    "revision": "aeb5303c56b0310c3e3cb1970124e61e",
+    "url": "/material-ui-design/static/media/pic3.aeb5303c.jpg"
+  },
+  {
+    "revision": "17d6a4338541fe46c6bb4704e68075ed",
+    "url": "/material-ui-design/static/media/pic4.17d6a433.jpg"
+  },
+  {
+    "revision": "76f09a495cee550dafd8ecf41a326c4a",
+    "url": "/material-ui-design/static/media/pic5.76f09a49.jpg"
+  },
+  {
+    "revision": "53dc1d23bb84b86ffb71cb85fbc2ab40",
+    "url": "/material-ui-design/static/media/pic6.53dc1d23.jpg"
+  },
+  {
+    "revision": "f02f5f1cc0ac46787c576c1785d37266",
+    "url": "/material-ui-design/static/media/pic7.f02f5f1c.jpg"
+  },
+  {
+    "revision": "5375cbcb7b2fc0847fef2882779eb93b",
+    "url": "/material-ui-design/static/media/pic8.5375cbcb.jpg"
+  },
+  {
+    "revision": "3ac009252a8dca63533d599bc272e88a",
+    "url": "/material-ui-design/static/media/pic9.3ac00925.jpg"
+  },
+  {
+    "revision": "013b5f9acba963f680381f8008270bd6",
+    "url": "/material-ui-design/static/media/resort1.013b5f9a.jpg"
+  },
+  {
+    "revision": "b732878b90ab99957abba447a64e27d1",
+    "url": "/material-ui-design/static/media/resort2.b732878b.jpg"
+  },
+  {
+    "revision": "54186286e5091d0889cc56babf3888ff",
+    "url": "/material-ui-design/static/media/resort3.54186286.jpg"
+  }
+]);
